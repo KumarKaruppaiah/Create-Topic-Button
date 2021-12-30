75b01e41d1f08d017395b699b412778c124f73d2
kumar.karuppaiah@in.abb.com <kumar.karuppaiah@in.abb.com>
2021-12-30 16:13:31 +0530

    changes

--- javascripts/discourse/api-initializers/init-test-component.js
+++ javascripts/discourse/api-initializers/init-test-component.js
@@ -1,6 +1,7 @@
 import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("0.8", (api) => {
+    debugger;
   api.createWidget("test-component-widget", {
     tagName: "div.test-component-widget",
 
